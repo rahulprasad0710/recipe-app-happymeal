a85a675410065b2694f380c0c16a867636e1fe96
rahulprasad0710 <raulshah2021@hotmail.com>
2021-04-12 05:01:56 +0530

    categry page add

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const Recipe = () => {
+  return (
+    <Layout>
+      <section className="page-recipe container-lg">this is About page</section>
+    </Layout>
+  )
+}
+
+export default Recipe
